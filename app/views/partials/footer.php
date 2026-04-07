a386d1cd3781bcdd260fec71c757f3fa5ac81df8
Malvino <malvinogarcia2@gmail.com>
2026-04-07 17:23:36 -0600

    feat: adicionando estrutura inicial do footer, header e nav, implementando os links do bootstrap

---
+++ app/views/partials/footer.php
@@ -0,0 +1,38 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://jsdelivr.net" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>footer</title>
+</head>
+<body>
+    
+    <footer class="container bg-primary-subtle p-4">
+        <div class="row">
+            <div class="col-12 col-md-auto">
+                <a href="#">Termos</a>
+            </div>
+
+            <div class="col-md-6">
+                <a href="">Privacidade</a>
+            </div>
+
+            <div class="col-md-6">
+                <a href="">Segurança</a>
+            </div>
+
+            <div>
+                <span>©  2026, DevDup Solutions. Todos os Direitos Reservados</span>
+            </div>
+        </div>
+    </footer>
+
+<script src="https://jsdelivr.net" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
