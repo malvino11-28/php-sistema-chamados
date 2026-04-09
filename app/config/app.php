5187cd0e2a2a3380b8cc1284c722e8a7280cf9bc
Olavo_Gsz <olavogz2007@gmail.com>
2026-04-08 21:49:41 -0300

    feat: Conexão com o banco de dados (MySql)

--- app/config/app.php
+++ app/config/app.php
@@ -1,6 +1,21 @@
 <?php
-    include 'config.php';
-    
-    $sql = "SELECT * from tickets";
-    $result = $conn->query($sql);
+   $host = "LocalHost";
+   $dbFlow = "Flow_Banco";
+   $user = "root";
+   $password = "";
+
+   try
+   {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbFlow;charset=utf8",$user, $password);
+
+    //Função de configuração de erros como exceção
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    echo "Conectado com Sucesso!!";
+   }
+   catch(PDOException $e)
+   {
+        echo "Erro na conexão: " . $e->getMessage();
+   }
+   
 ?>
